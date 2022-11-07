6c9b465f2a3ac664cf26a5585dc0ff8f4efc33bc
Enzo Bognar <110871103+EnzoBognar@users.noreply.github.com>
2022-11-07 15:44:17 -0300

    Add files via upload
    Trabalho 2 - Linguagem C# - Classe encapsulada
    
    para utilizar baixe como arquivo .zip e abra com Visual Studio.

---
+++ Trabalho 2/Funcionarios.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace TabelaFuncionarios
+{
+
+    public class Funcionario
+    {
+        private string Nome;
+        private string Matricula;
+        private double Salario;
+
+        public Funcionario(string Nome, string Matricula, double Salario)
+        {
+            this.Nome = Nome;
+            this.Matricula = Matricula;
+            this.Salario = Salario;
+
+        }
+
+
+        public string getNome()
+        {
+            return this.Nome;
+        }
+        public string getMatricula()
+        {
+            return this.Matricula;
+        }
+        public double getSalario()
+        {
+            return this.Salario;
+        }
+
+
+        public void setNome(string Nome)
+        {
+            this.Nome = Nome;
+        }
+        public void setMatricula(string Matricula)
+        {
+            this.Matricula = Matricula;
+        }
+        public void setSalario(double Salario)
+        {
+            this.Salario = Salario;
+        }
+
+        
+        
+    }
+}
---
+++ Trabalho 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http.Headers;
+using TabelaFuncionarios;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario[] funci = {
+            new Funcionario("José", "1432559", 1500.4),
+            new Funcionario("Enzo", "8123357", 1950.3),
+            new Funcionario("Erick", "8732432", 2300.2),
+            new Funcionario("Carlos", "3254331", 1300.5)
+        };
+
+            double Media = 1762.85;
+
+
+
+           for(int i = 0; i < 4; i++)
+            {
+                if ((funci[i].getSalario()) > Media)
+                {
+                    Console.WriteLine(funci[i].getNome());
+                    Console.WriteLine(funci[i].getMatricula());
+                    Console.WriteLine("");
+                }
+            }
+           
+
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}

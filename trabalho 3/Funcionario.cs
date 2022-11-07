709a5f24bf028e04e3cd111bb02fe6b5294aa648
Enzo Bognar <110871103+EnzoBognar@users.noreply.github.com>
2022-11-07 15:54:24 -0300

    Add files via upload
    Trabalho 3 de C#
    
    Para utilizar baixe a pasta e execute com o Visual Studio o arquivo .cs

---
+++ trabalho 3/Funcionario.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalho_3
+{
+    class Funcionario
+    {
+        private string nome;
+        private int matricula;
+        private double salario;
+
+        public Funcionario(string nome, int matricula, double salario)
+        {
+            this.Nome = nome;
+            this.Matricula = matricula;
+            this.Salario = salario;
+
+        }
+
+        public Funcionario()
+        {
+
+        }
+
+        public Funcionario(string nome)
+        {
+            this.nome = nome;
+        }
+
+
+        public string Nome
+        {
+            set
+            {
+                nome = value;
+            }
+            get
+            {
+                return nome;
+            }
+
+        }
+
+        public int Matricula
+        {
+            set
+            {
+                matricula = value;
+            }
+            get
+            {
+                return matricula;
+            }
+        }
+
+        public double Salario
+        {
+            set
+            {
+                salario = value;
+            }
+            get
+            {
+                return salario;
+            }   
+        }
+    }
+}
---
+++ trabalho 3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using trabalho_3;
+
+namespace trabalho_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            double soma = 0;
+            
+            Funcionario[] funci = {new Funcionario("Enzo", 143234, 2543.32),
+            new Funcionario("Carlos", 143243, 2764.76),
+            new Funcionario("Erick", 324534, 2349.54),
+            new Funcionario("Denise", 945326, 2951.43)
+            };
+
+            for(i = 0; i < 4; i++)
+            {
+                soma = soma + funci[i].Salario;
+            }
+
+            double media = soma / i;
+
+            for (i = 0; i < 4; i++)
+            {
+                if (funci[i].Salario > media)
+                {
+                    Console.WriteLine(funci[i].Nome + " " + funci[i].Matricula);
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
